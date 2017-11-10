236d802c58f50c7e598543e83838a086d231e3d9
Haris Skiadas <cskiadas@gmail.com>
2017-11-10 14:22:51 -0500

    Sample of  module structure

---
+++ Game.hs
@@ -0,0 +1,17 @@
+{-|
+Module      : Game
+Description : The Game module
+Copyright   : (c) Some Guy, 2013
+                  Someone Else, 2014
+License     : MIT
+Maintainer  : sample@email.com
+
+Here is a longer description of this module, containing some
+commentary with @some markup@. FIX THIS
+-}
+module Game
+(
+
+) where
+
+-- Module starts here.
