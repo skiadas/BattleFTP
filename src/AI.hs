24be9733b1a6ddb58231a2ee5f93b4c844178e89
walkert <walkert20@hanover.edu>
2017-11-17 15:52:15 -0500

    update AI module.

--- src/AI.hs
+++ src/AI.hs
@@ -8,14 +8,32 @@ Maintainer  : sample@email.com
 
 Here is a longer description of this module, containing some
 commentary:
-	This module is for the brains of all AI units within 
-	the game, providing all AI units with actions and strategies
-	for combating the UI units. This module is mostly internal.
+    This module is for the brains of all AI units within 
+    the game, providing all AI units with actions and strategies
+    for combating the UI units. This module is mostly internal. (Update at some point)
 
 -}
 module AI
 (
+    -- other modules
+    module Unit,
+    module Combat,
+    -- ** A data type
+    AI
+    -- some functions
 
 ) where
 
 -- Module starts here.
+
+import Combat
+import Unit
+
+data AI = Unit -> Action 
+
+main:: IO a
+main = error "nothing now."
+      --do
+      -- all of the gets
+      -- do stuff based on stats
+      --return something
