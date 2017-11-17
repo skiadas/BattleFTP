909e9584f7ecf48fc506d52b1e9c26a2646742f8
Hannah Mundt <mundth19@hanover.edu>
2017-11-17 15:59:07 -0500

    describe state module

--- src/State.hs
+++ src/State.hs
@@ -3,11 +3,21 @@ Module      : State
 Description : The State module
 Copyright   : (c) Some Guy, 2013
                   Someone Else, 2014
+Copyright   : (c) Jonathan Helmers, 2017
+                  Hannah Mundt, 2017
 License     : MIT
 Maintainer  : sample@email.com
+Maintainer  : mundth19@hanover.edu
 
 Here is a longer description of this module, containing some
 commentary with @some markup@. FIX THIS
+This module collects all data and returns it to game.
+It will use organizational data types to maintain 
+data from other groups.
+Maintains, updates and stores data from environment,
+unit, and inventory.
+
+
 -}
 module State
 (
