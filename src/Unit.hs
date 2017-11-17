81423a07527c23918ff21dca3cac033c84995d90
Jacob Hedrick <jacobhedrick1296@gmail.com>
2017-11-17 15:29:13 -0500

    Added Clearer Documentation

--- src/Unit.hs
+++ src/Unit.hs
@@ -1,4 +1,4 @@
-{-|
+
 Module      : Unit
 Description : The Unit module
 Copyright   : (c) Collin Umphrey, 2017
@@ -10,11 +10,12 @@ Here is a longer description of this module, containing some
 commentary with @some markup@. 
 
 Basic and advanced parts of units, including but not limited to:
-Stats
-Levels
+Stats - HP, Attack, Defense, Speed, Intelligence(power of magic and defense of magic(maybe if we do magic))
+Levels - Stat growths through levels, what level would be max, current level and experience.
+Abilities - 
+Classes - What each class can do
+
 Attributes
-Abilities
-Classes
 
 
 
