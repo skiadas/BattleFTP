7ae2d0363bfcdea23522caee2542e02eb01ca116
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2017-11-17 15:51:31 -0500

    Fixed Documentation

--- src/Unit.hs
+++ src/Unit.hs
@@ -3,11 +3,21 @@ Module      : Unit
 Description : The Unit module
 Copyright   : (c) Collin Umphrey, 2017
                   Graham Wood, 2017
-License     : MIT
-Maintainer  : sample@email.com
+License     : Hanover College
+Maintainer  : umphreyc19@hanover.edu
 
 Here is a longer description of this module, containing some
-commentary with @some markup@. FIX THIS
+commentary with @some markup@. 
+
+Basic and advanced parts of units, including but not limited to:
+Stats
+Levels
+Attributes
+Abilities
+Classes
+
+
+
 -}
 module Unit
 (
