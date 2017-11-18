1d571895cfe9f1bed41e9fee724f2b6d1367ba16
Jacob Hedrick <jacobhedrick1296@gmail.com>
2017-11-18 09:09:37 -0500

    Inventory Module Description
    Added a brief description of the Inventory Module to the documentation of the Inventory.hs file

--- src/Inventory.hs
+++ src/Inventory.hs
@@ -7,7 +7,13 @@ License     : MIT
 Maintainer  : sample@email.com
 
 Here is a longer description of this module, containing some
-commentary with @some markup@. FIX THIS
+commentary:
+	This module creates and controls all items in the game. It will be used to create
+	different classes of items (weapons, shields, armor, etc.), the different items
+	in each class, and assign attrbutes to each of these items. We will also create and
+	maintain the UI storage of items. This module is mainly opaque with many methods to 
+	be used by other teams.
+
 -}
 module Inventory
 (
