336be7f7874eaeeabb26eeea9b5546ece16e6713
Unknown <haflg19@hanover.edu>
2017-11-27 15:57:08 -0500

    stub data types

--- src/Environment.hs
+++ src/Environment.hs
@@ -5,6 +5,7 @@ Copyright   : (c) Gage Hafl, 2017
                   Terriyon Veasy, 2017
 License     : MIT
 Maintainer  : haflg19@hanover.edu
+              veasyt19@hanover.edu
 
 Here is a longer description of this module, containing some
 commentary with @some markup@. FIX THIS
@@ -12,6 +13,15 @@ commentary with @some markup@. FIX THIS
 module Environment
 (
 
+
 ) where
 
 -- Module starts here.
+
+data Weather = Rain | Wind | Snow | Clear
+data Type = B | I | O {- B = boundary, I = inside, O = outside -}
+data Floor = 1 | 2 | 3
+
+type Map = [[Block]] {- Grid Map, each Block is a place the player can move to -}
+type Block = (Type, Up, Left, Down, Right) {- (Current Type, Type if Up, Type if Right, Type if Down, Type if Left) -}
+type Building = (String, Floor) {- (Name, Floor) -}
