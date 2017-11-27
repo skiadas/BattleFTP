86c87c8d3026b42e67ba801a98a266354c2b111b
amartens181 <a.martens181@gmail.com>
2017-11-27 14:02:13 -0500

    Update UI module information

--- src/UI.hs
+++ src/UI.hs
@@ -1,13 +1,20 @@
 {-|
 Module      : UI
 Description : The UI module
-Copyright   : (c) Some Guy, 2013
-                  Someone Else, 2014
-License     : MIT
-Maintainer  : sample@email.com
+Copyright   : (c) Alexander Martens, 2017
+                  Patrick Conboy, 2017
+License     : MIT ( (BSD-3) )
+Maintainer  : support@hanover.edu
 
-Here is a longer description of this module, containing some
-commentary with @some markup@. FIX THIS
+
+The UI module communicates with the user by writing fancy ANSI graphics to 
+the terminal and accepting user input in the form of text. The UI module is 
+reponsible for all interaction with the user. The UI module shall call other
+modules in accordance with the terms. No other module shall interfere with user
+input and no other module may ping the UI module to do anything. The user is the
+only one the application servers. (((Google)))
+Random events for any reason will be triggered by the UI and the UI will always supply
+sufficient information to for the rest of the application. 
 -}
 module UI
 (
