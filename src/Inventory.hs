3582288adcbbf645cf1110bf79dc04eed030bb65
Jacob Hedrick <jacobhedrick1296@gmail.com>
2017-11-27 15:23:30 -0500

    Fixed Spelling Error for Shield
    Changed "Sheild" to "Shield"

--- src/Inventory.hs
+++ src/Inventory.hs
@@ -32,5 +32,5 @@ data ItemType = Weapon|Shield|Armour
 
 --Pre-made items
 sword = ("sword", Weapon, 0, 2, 1, 0) :: Item
-sheild = ("sheild", Sheild, 0, 1, 3, -1) :: Item
+shield = ("shield", Shield, 0, 1, 3, -1) :: Item
 armour = ("armour", Armour, 3, 0, 0, -2) :: Item
