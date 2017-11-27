7f1f29dbf0e9f173a60447310515880d10a87c90
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2017-11-27 15:12:01 -0500

    Added function to create Units

--- src/Unit.hs
+++ src/Unit.hs
@@ -22,17 +22,66 @@ Attributes
 -}
 module Unit
 (
-	
+
     type Name = String
     type HP = Int
-    type Attack = Int
-    type Defense = Int
-    type Speed = Int
-	data Unit = (Name, HP, Attack, Defense, Speed, Death, pType)
-	data Death = False | True
-	data IsPlayer = False | True
+    type Attack = Int --A stat that influences damage done in battle
+    type Defense = Int --A stat that influences how much damage is taken in battle
+    type Speed = Int --A stat that influences avoid chance and turn order
+    data Attributes (HP,Attack,Defense,Speed)
+	data Unit = (Name, Attributes, Death, pType) --The collection of important stats
+	data Death = False | True --Is a unit dead or not. Defaults to alive
+	data IsPlayer = False | True --Is it a player or not. Defaults to Enemy
 
 
 ) where
 
 -- Module starts here.
+
+--Given a unit returns that unit's name
+getName :: Unit -> String
+getName (unitName, _, _, _, _, _, _) = unitName
+
+--given a unit's attributes, returns its HP
+getHealth :: Attributes -> Int
+getName (hp, _, _, _) = hp
+
+--given a unit's attributes, returns its Attack
+getAttack :: Attributes -> Int
+getAttack (_, atk, _, _) = atk
+
+--given a unit's attributes, returns its Defense
+getDefense :: Attributes -> Int
+getDefense (_, _, def, _) = def
+
+--given a unit's attributes, returns its Speed
+getSpeed :: Attributes -> Int 
+getSpeed (_, _, _, spd) = spd
+
+createUnit :: String -> Int -> Int -> Int -> Int -> Bool -> Unit
+createUnit unitName maxHP atk def spd player = (unitName, (maxHP, atk, def, spd), False, player)
+
+--given a set of attributes, and a new HP stat, returns those attributes with the updated stat
+changeHealth :: Attributes -> Int -> Attributes
+createHeatlh (oldHP, atk, def, spd) newHP = (newHP, atk, def, spd)
+
+--given a set of attributes, and a new Attack, returns those attributes with the updated stat
+changeHealth :: Attributes -> Int -> Attributes
+createHeatlh (hp, oldAtk, def, spd) newAtk = (hp, newAtk, def, spd)
+
+--given a set of attributes, and a new Defense, returns those attributes with the updated stat
+changeHealth :: Attributes -> Int -> Attributes
+createHeatlh (hp, atk, oldDef, spd) newDef = (hp, atk, newDef, spd)
+
+--given a set of attributes, and a new Speed, returns those attributes with the updated stat
+changeHealth :: Attributes -> Int -> Attributes
+createHeatlh (hp, atk, def, oldSpd) newSpd = (hp, atk, def, newSpd)
+
+
+
+
+
+
+
+
+
