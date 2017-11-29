ebeecc261b6b6fac1b8b8701f6d61aaa17b3aeec
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-11-29 15:19:58 -0500

    Create an Action file for available actions of a unit

---
+++ src/Combat/Action.hs
@@ -0,0 +1,21 @@
+{-|
+Module      : Combat
+Description : The Combat module
+Copyright   : (c) Kenny Jarnagin, 2017
+                  Lucas Miller, 2017
+License     : MIT
+Maintainer  : jarnagink19@hanover.edu
+
+Here is a longer description of this module, containing some
+commentary with @some markup@. 
+-}
+
+--import Unit FIX THIS
+module Combat.Action
+(
+	Action
+) where
+
+data Action = Attack (Unit, Unit) | Heal (Unit, Unit)
+
+
