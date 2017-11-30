532ab724bff3d0a3dce74fe816ba964c78eac657
Hannah Mundt <mundth19@hanover.edu>
2017-11-30 13:27:27 -0500

    wrote preliminary JSON functions

--- src/State.hs
+++ src/State.hs
@@ -1,16 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-|
 Module      : State
 Description : The State module
-Copyright   : (c) Some Guy, 2013
-                  Someone Else, 2014
 Copyright   : (c) Jonathan Helmers, 2017
                   Hannah Mundt, 2017
 License     : MIT
-Maintainer  : sample@email.com
 Maintainer  : mundth19@hanover.edu
 
-Here is a longer description of this module, containing some
-commentary with @some markup@. FIX THIS
 This module collects all data and returns it to game.
 It will use organizational data types to maintain 
 data from other groups.
@@ -19,9 +15,62 @@ unit, and inventory.
 
 
 -}
-module State
-(
 
-) where
+import Data.Aeson
+--import Unit.hs
+--import Environment.hs
+--import Inventory.hs
+import GHC.Generics
+
+
+--module State
+--(
+
+--) where
 
 -- Module starts here.
+
+data JSONUnit = JSONUnit {
+    username :: String--Username
+    , hp :: Int--HP
+    , attack :: Int--Attack
+    , defense :: Int--Defense
+    , speed :: Int --Speed
+ } deriving (Generic, Show)
+
+data JSONEnvironment = JSONEnvironment {
+    map :: Map
+    , block :: Block
+    , building :: Building
+    --weather??    
+ } deriving (Generic, Show)
+
+data JSONInventory = JSONInventory {
+    item :: Item 
+ } deriving (Generic, Show) 
+
+data JSONState = JSONState {
+    JSONUnit :: --
+    JSONEnvironment :: --
+    JSONInventory :: --
+}
+
+instance ToJSON JSONUnit where
+    toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON JSONUnit
+
+instance ToJSON JSONEnvironment where
+    toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON JSONEnvironment
+
+instance ToJSON JSONInventory where
+    toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON JSONInventory
+
+instance ToJSON JSONState where
+    toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON JSONState
