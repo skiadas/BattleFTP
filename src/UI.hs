181811e3bf3177252835c3873215dad5f3e60bc4
amartens181 <a.martens181@gmail.com>
2017-11-29 22:25:21 -0500

    UI start

--- src/UI.hs
+++ src/UI.hs
@@ -14,11 +14,49 @@ modules in accordance with the terms. No other module shall interfere with user
 input and no other module may ping the UI module to do anything. The user is the
 only one the application servers. (((Google)))
 Random events for any reason will be triggered by the UI and the UI will always supply
-sufficient information to for the rest of the application. 
+sufficient information to for the rest of the application.
 -}
-module UI
-(
+-- module UI
+-- (
 
-) where
+-- ) where
 
 -- Module starts here.
+import Prelude hiding (Either(..))
+import System.Console.ANSI
+import System.IO
+
+main :: IO()
+main = do 
+    setTitle "Battle! For The Point"
+    hSetEcho stdin False
+    hSetBuffering stdin  NoBuffering
+    hSetBuffering stdout NoBuffering
+    hideCursor
+    setSGR [ SetConsoleIntensity BoldIntensity
+            , SetColor Foreground Vivid Blue
+            , SetColor Background Vivid Black ]
+    drawScreen
+    handleExit
+
+drawScreen :: IO()
+drawScreen = do
+  clearScreen
+  drawString ("I like food... maybe potions...", 0,0)
+  drawString ("I like food... maybe potions...", 1,0)
+  drawString ("I like food... maybe potions...", 2,0)
+  drawString ("x3...", 3,0)
+
+drawString :: (String, Int, Int) -> IO()
+drawString (string, x, y) = do
+  setCursorPosition x y
+  putStr string
+
+  -- when the user wants to exit we give them a thank you
+  -- message and then reshow the cursor
+handleExit :: IO()
+handleExit = do
+    clearScreen
+    setCursorPosition 0 0
+    showCursor
+    putStrLn "Thank you for playing!"
