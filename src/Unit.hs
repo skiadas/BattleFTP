0058cbf2ea88a36775203280cd00e7bee43cf1c5
Collin Beauchamp-Umphrey <umphreyc19@hanover.edu>
2017-12-01 15:38:26 -0500

    Added MaxHP to the attributes of a Unit
    Also implemented a getMaxHP for other groups to use

--- src/Unit.hs
+++ src/Unit.hs
@@ -21,7 +21,7 @@ Attributes
 
 -}
 
-import Inventory
+
 module Unit
 (
  createUnit,
@@ -31,6 +31,7 @@ module Unit
  changeSpeed,
  getName,
  getHealth,
+ getMaxHP,
  getAttack,
  getDefense,
  getSpeed
@@ -40,10 +41,11 @@ module Unit
 
 type Username = String
 type HP = Int
+type MaxHP = Int
 type Attack = Int --A stat that influences damage done in battle
 type Defense = Int --A stat that influences how much damage is taken in battle
 type Speed = Int --A stat that influences avoid chance and turn order
-data Attributes = Attributes (HP,Attack,Defense,Speed)
+data Attributes = Attributes (HP,MaxHP,Attack,Defense,Speed)
 data Unit = Unit (Username, Attributes, Death, IsPlayer) --The collection of important stats
 type Death = Bool --Is a unit dead or not. Defaults to alive
 type IsPlayer = Bool  --Is it a player or not. Defaults to Enemy
@@ -56,40 +58,42 @@ getName (Unit (pName, _, _, _)) = pName
 
 --given a unit's attributes, returns its HP
 getHealth :: Unit -> Int
-getHealth (Unit (_, Attributes(hp, _, _, _) ,_ ,_)) = hp
+getHealth (Unit (_, Attributes(hp, _, _, _, _) ,_ ,_)) = hp
 
+getMaxHP :: Unit -> Int
+getMaxHP (Unit (_, Attributes(_, maxHP, _, _, _) ,_ ,_)) = maxHP
 --given a unit's attributes, returns its Attack
 getAttack :: Unit -> Int
-getAttack (Unit (_, Attributes(_, atk, _, _) ,_ ,_)) = atk
+getAttack (Unit (_, Attributes(_, _, atk, _, _) ,_ ,_)) = atk
 
 --given a unit's attributes, returns its Defense
 getDefense :: Unit -> Int
-getDefense (Unit (_, Attributes(_, _, def, _) ,_ ,_)) = def
+getDefense (Unit (_, Attributes(_, _, _, def, _) ,_ ,_)) = def
 
 --given a unit's attributes, returns its Speed
 getSpeed :: Unit -> Int 
-getSpeed (Unit (_, Attributes(_, _, _, spd) ,_ ,_)) = spd
+getSpeed (Unit (_, Attributes(_, _, _, _, spd) ,_ ,_)) = spd
 
 
 
 createUnit :: Username -> HP -> Attack -> Defense -> Speed -> IsPlayer -> Unit
-createUnit unitName maxHP atk def spd player = (Unit (unitName, (Attributes (maxHP, atk, def, spd)), False, player))
+createUnit unitName maxHP atk def spd player = (Unit (unitName, (Attributes (maxHP, maxHP, atk, def, spd)), False, player))
 
 --given a set of attributes, and a new HP stat, returns those attributes with the updated stat
 changeHealth :: Unit -> Int -> Unit
-changeHealth (Unit (name, (Attributes (oldHP, atk, def, spd)), death, player)) newHP = (Unit (name, Attributes (newHP, atk, def, spd), death, player))
+changeHealth (Unit (name, (Attributes (oldHP, maxHP, atk, def, spd)), death, player)) newHP = (Unit (name, Attributes (newHP, maxHP, atk, def, spd), death, player))
 
 --given a set of attributes, and a new Attack, returns those attributes with the updated stat
 changeAttack :: Unit -> Int -> Unit
-changeAttack (Unit ( name, (Attributes (hp, oldAtk, def, spd)), death, player)) newAtk = (Unit ( name, Attributes (hp, newAtk, def, spd), death, player))
+changeAttack (Unit ( name, (Attributes (hp,maxHP, oldAtk, def, spd)), death, player)) newAtk = (Unit ( name, Attributes (hp,maxHP, newAtk, def, spd), death, player))
 
 --given a set of attributes, and a new Defense, returns those attributes with the updated stat
 changeDefense :: Unit -> Int -> Unit
-changeDefense (Unit ( name, (Attributes (hp, atk, oldDef, spd)), death, player)) newDef = (Unit ( name, Attributes (hp, atk, newDef, spd), death, player))
+changeDefense (Unit ( name, (Attributes (hp,maxHP, atk, oldDef, spd)), death, player)) newDef = (Unit ( name, Attributes (hp,maxHP, atk, newDef, spd), death, player))
 
 --given a set of attributes, and a new Speed, returns those attributes with the updated stat
 changeSpeed :: Unit -> Int -> Unit
-changeSpeed (Unit (name, (Attributes (hp, atk, def, oldSpd)), death, player)) newSpd = (Unit (name, Attributes (hp, atk, def, newSpd), death, player))
+changeSpeed (Unit (name, (Attributes (hp,maxHP, atk, def, oldSpd)), death, player)) newSpd = (Unit (name, Attributes (hp,maxHP, atk, def, newSpd), death, player))
 
 
 
