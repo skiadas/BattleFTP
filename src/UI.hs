63e04085989af79281503deeadaef06f6eb22ea5
amartens181 <a.martens181@gmail.com>
2017-12-01 14:19:35 -0500

    Make A GameLoop

--- src/UI.hs
+++ src/UI.hs
@@ -25,6 +25,7 @@ sufficient information to for the rest of the application.
 import Prelude hiding (Either(..))
 import System.Console.ANSI
 import System.IO
+import Data.Char
 
 data Command = Empty
             |Move
@@ -62,40 +63,26 @@ stringToSubCom _ = Void
 readCom :: [String] -> (Command, SubCommand)
 readCom s = (s', s'')
     where   s' = stringToCom (head s)
-            s'' = stringToSubCom (s!!1)
-
-main :: IO()
-main = do 
-    setTitle "Battle! For The Point"
-    --hSetEcho stdin False --how do we accept input?
-    ---hSetBuffering stdin  NoBuffering --sounds like we want buffering
-    --hSetBuffering stdout NoBuffering --sounds like we want buffering
-    hideCursor
-    setSGR [ SetConsoleIntensity BoldIntensity
-            , SetColor Foreground Vivid Blue
-            , SetColor Background Vivid Black ]
-    drawScreen
-    handleExit --just exit after first draw for now...
-
---read :: 
+            s'' = if length s > 1 then stringToSubCom (s !! 1) else stringToSubCom ""
 
 --Method to change user input to commands
 processCommand :: String -> (Command, SubCommand)
-processCommand s = readCom s'
-                    where s' = words s
+processCommand s = readCom (words (map toLower s))
 
 --Method to execute commands
-executeCommand :: Command -> SubCommand -> IO()
-executeCommand Move F = drawScreen
-executeCommand Move B = drawScreen
-executeCommand Move L = drawScreen
-executeCommand Move R = drawScreen
-executeCommand I Show = drawScreen
-executeCommand Use Heal = drawScreen
-executeCommand Pickup Void = drawScreen
-executeCommand Exit Void = drawScreen
+executeCommand :: (Command, SubCommand) -> IO()
+executeCommand (Move, F) = drawScreen
+executeCommand (Move, B) = drawScreen
+executeCommand (Move, L) = drawScreen
+executeCommand (Move, R) = drawScreen
+executeCommand (I, Show) = drawScreen
+executeCommand (Use, Heal) = drawScreen
+executeCommand (Pickup, Void) = drawScreen
+executeCommand (Exit, Void) = handleExit
+executeCommand (_, _) = drawScreen
 
 --Method to draw all information to the screen...
+--Where all the UI magics will be made 😁
 drawScreen :: IO()
 drawScreen = do
     clearScreen
@@ -113,11 +100,41 @@ drawString (string, x, y) = do
     setCursorPosition x y
     putStr string
 
+main :: IO()
+main = do 
+    setTitle "Battle! For The Point"
+    --hSetEcho stdin False --how do we accept input?
+    --hSetBuffering stdin  NoBuffering --sounds like we want buffering
+    --hSetBuffering stdout NoBuffering --sounds like we want buffering
+    --hideCursor
+    setSGR [ SetConsoleIntensity BoldIntensity
+            , SetColor Foreground Vivid Blue
+            , SetColor Background Dull Black ]
+    drawScreen
+    gameLoop
+    
+
+-- update the game loop to add in the goodbye message
+gameLoop :: IO()
+gameLoop = do
+    putStrLn "\n"
+    putStrLn "Enter The Command, Sir: "
+    commands <- getLine
+    let comTuple = processCommand commands
+
+    executeCommand comTuple
+    case comTuple of
+        (Exit, Void) -> handleExit
+        _    -> gameLoop
+
   -- when the user wants to exit we give them a thank you
-  -- message and then reshow the cursor
+  -- message and reset the terminal
 handleExit :: IO()
 handleExit = do
+    setSGR [Reset]
     clearScreen
     setCursorPosition 0 0
     showCursor
-    putStrLn "Thank you for playing!"
+    putStrLn "Thank you for playing!!!"
+    --putStrLn "\xF0\x9F\x98\x82" 
+    putStrLn "😂😂😂"
