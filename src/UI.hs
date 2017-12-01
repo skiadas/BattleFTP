2df67303b319162de7dc0ba30bd5f4145e43e459
amartens181 <a.martens181@gmail.com>
2017-12-01 10:24:43 -0500

    Add String Interpretation

--- src/UI.hs
+++ src/UI.hs
@@ -26,31 +26,92 @@ import Prelude hiding (Either(..))
 import System.Console.ANSI
 import System.IO
 
+data Command = Empty
+            |Move
+            |I
+            |Use
+            |Pickup
+            |Exit
+
+data SubCommand = Void
+                |F
+                |B
+                |L
+                |R
+                |Show
+                |Heal
+
+
+stringToCom :: String -> Command
+stringToCom "move" = Move
+stringToCom "i" = I
+stringToCom "use" = Use
+stringToCom "pickup" = Pickup
+stringToCom "exit" = Exit
+stringToCom _ = Empty
+
+stringToSubCom :: String -> SubCommand
+stringToSubCom "f" = F
+stringToSubCom "b" = B
+stringToSubCom "l" = L
+stringToSubCom "r" = R
+stringToSubCom "show" = Show
+stringToSubCom "food" = Heal --food?
+stringToSubCom _ = Void
+
+readCom :: [String] -> (Command, SubCommand)
+readCom s = (s', s'')
+    where   s' = stringToCom (head s)
+            s'' = stringToSubCom (s!!1)
+
 main :: IO()
 main = do 
     setTitle "Battle! For The Point"
-    hSetEcho stdin False
-    hSetBuffering stdin  NoBuffering
-    hSetBuffering stdout NoBuffering
+    --hSetEcho stdin False --how do we accept input?
+    ---hSetBuffering stdin  NoBuffering --sounds like we want buffering
+    --hSetBuffering stdout NoBuffering --sounds like we want buffering
     hideCursor
     setSGR [ SetConsoleIntensity BoldIntensity
             , SetColor Foreground Vivid Blue
             , SetColor Background Vivid Black ]
     drawScreen
-    handleExit
+    handleExit --just exit after first draw for now...
 
+--read :: 
+
+--Method to change user input to commands
+processCommand :: String -> (Command, SubCommand)
+processCommand s = readCom s'
+                    where s' = words s
+
+--Method to execute commands
+executeCommand :: Command -> SubCommand -> IO()
+executeCommand Move F = drawScreen
+executeCommand Move B = drawScreen
+executeCommand Move L = drawScreen
+executeCommand Move R = drawScreen
+executeCommand I Show = drawScreen
+executeCommand Use Heal = drawScreen
+executeCommand Pickup Void = drawScreen
+executeCommand Exit Void = drawScreen
+
+--Method to draw all information to the screen...
 drawScreen :: IO()
 drawScreen = do
-  clearScreen
-  drawString ("I like food... maybe potions...", 0,0)
-  drawString ("I like food... maybe potions...", 1,0)
-  drawString ("I like food... maybe potions...", 2,0)
-  drawString ("x3...", 3,0)
+    clearScreen
+    drawString ("I like food... maybe potions...", 0,0)
+    drawString ("I like food... maybe potions...", 1,0)
+    drawString ("I like food... maybe potions...", 2,0)
+    drawString ("x3...", 3,0)
 
+--Method to draw a string at a certain position on the screen
+--Params: string: string to draw
+--Params: x: where to draw 
+--Params: y: where to draw
 drawString :: (String, Int, Int) -> IO()
 drawString (string, x, y) = do
-  setCursorPosition x y
-  putStr string
+    setCursorPosition x y
+    putStr string
 
   -- when the user wants to exit we give them a thank you
   -- message and then reshow the cursor
