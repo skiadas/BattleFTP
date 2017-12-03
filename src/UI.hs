147565844eb032b5dba7b3c20c564e0f00623181
amartens181 <a.martens181@gmail.com>
2017-12-03 13:26:05 -0500

    Never saw this...

--- src/UI.hs
+++ src/UI.hs
@@ -4,19 +4,17 @@ Description : The UI module
 Copyright   : (c) Alexander Martens, 2017
                   Patrick Conboy, 2017
 License     : MIT ( (BSD-3) )
-Maintainer  : support@hanover.edu
-
+Maintainer  : conboyp19@hanover.edu
 
 The UI module communicates with the user by writing fancy ANSI graphics to 
 the terminal and accepting user input in the form of text. The UI module is 
 reponsible for all interaction with the user. The UI module shall call other
 modules in accordance with the terms. No other module shall interfere with user
-input and no other module may ping the UI module to do anything. The user is the
-only one the application servers. (((Google)))
+input, and no other module may ping the UI module to do anything. The user is the
+only one the application serves. (((Google)))
 Random events for any reason will be triggered by the UI and the UI will always supply
-sufficient information to for the rest of the application.
+sufficient information for the rest of the application.
 -}
--- module UI
 -- (
 
 -- ) where
