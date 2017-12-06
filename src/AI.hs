91bf27b51bf3a482ba354675c0365d45738b9b06
walkert <walkert20@hanover.edu>
2017-12-06 15:37:39 -0500

    Update Ai module.

--- src/AI.hs
+++ src/AI.hs
@@ -4,22 +4,30 @@ Description : The AI module
 Copyright   : (c) Brian Hayes,
                   Tyler Walker, 2017
 License     : MIT
-Maintainer  : sample@email.com
+Maintainer  : walkert20@hanover.edu
 
 Here is a longer description of this module, containing some
 commentary:
     This module is for the brains of all AI units within 
     the game, providing all AI units with actions and strategies
-    for combating the UI units. This module is mostly internal. (Update at some point)
+    for combating the UI units. This module is mostly internal. 
 
+    findLowstHp is a function that returns 
+    the AI unit with the least health within a given 
+    list of either Ai or Ui units.
+
+    lessThan50PercentHp is a function that 
+    looks for a unit whose hp<=50% out of the 
+    given list of Ai/Ui units.
+
+    choseAction s a function that returns 
+    an Action for the AI unit to perform. 
 -}
 module AI
 (
     -- ** A data type
-    AI, -- exporting module
     -- some functions
-    findLowestHp,
-    isBoss
+    chooseAction
 ) where
 
 -- Module starts here.
@@ -29,6 +37,7 @@ import System.Random
 import Unit
 
 -- A function that returns the AI unit with the least health.
+-- [Unit] can either be the Ai units or the player units.
 findLowestHp:: [Unit] -> Unit
 findLowestHp (x:[])                  = x
 findLowestHp (x,y:rest) 
@@ -36,15 +45,8 @@ findLowestHp (x,y:rest)
         |getHealth x == getHealth y  = findLowestHp (x:rest)
         |otherwise                   = findLowestHp (y:rest)
 
--- A function that looks for an Ai unit whose hp<=75%
-lessThan75PercentHp:: [Unit] -> Bool
-lessThan75PercentHp []        = False
-lessThan75PercentHp (x:xs)
-    |getHealth x <= fourth    = True
-    |otherwise                = lessThan75PercentHp (xs)
-        where fourth = (3*(getMaxHP x))/4
-
--- A function that looks for an Ai unit whose hp<=50%
+-- A function that looks for a unit whose hp<=50%
+-- [Unit] can either be the Ai units or the player units.
 lessThan50PercentHp:: [Unit] -> Bool
 lessThan50PercentHp []      = False
 lessThan50PercentHp (x:xs)
@@ -53,6 +55,9 @@ lessThan50PercentHp (x:xs)
         where half = (getMaxHP x)/2
 
 -- A function that returns an Action for the AI to perform. 
+-- Unit is the current Ai whose turn it is.
+-- 1st [Unit] is the Ai units, including the turn Ai.
+-- 2nd [Unit] is the Ui/player units.
 chooseAction:: Unit -> [Unit] -> [Unit] -> IO Action
 chooseAction ai (ais:friends) (player:rest) = do
         x <- rollDice
@@ -62,14 +67,7 @@ chooseAction ai (ais:friends) (player:rest) = do
                 where allie = findLowestHp (ai:ais:friends)
                       enemy = findLowestHp (player:rest)
 
--- A function that returns an Action for a boss unit AI to perform, if there is one.
-chooseBossAction:: Unit -> [Unit] -> IO Action
-chooseBossAction self (player:rest) = error "stub"
-
--- A function that checks if a given Unit is a normal unit or a boss unit.
-isBoss:: Unit 
-isBoss ai = error "stub"
-
--- A helper function for chooseAction 
+-- A helper function for chooseAction.
+-- returns a random number betwee 1 and 100. 
 rollDice :: IO Int
 rollDice = getStdRandom (randomR (1,100))
